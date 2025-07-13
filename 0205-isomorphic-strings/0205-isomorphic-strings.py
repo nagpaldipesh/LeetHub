86619091073b4f36c317d27facd114e5fdb9c08e
nagpaldipesh <93336445+nagpaldipesh@users.noreply.github.com>
2025-07-13 08:23:17 +0530

    Time: 10 ms (18.04%), Space: 17.8 MB (95.54%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -4,19 +4,36 @@ class Solution:
         if(n != len(t)):
             return False
 
-        maps = {}
-        mapt = {}
-
+        maps = [-1] * 128
+        mapt = [-1] * 128
+        sub = ord('a')
+        
         for i in range(n):
-            key1 = s[i]
-            key2 = t[i]
-            
-            if key1 in maps and maps[key1] != key2:
+            key1 = ord(s[i]) - sub
+            key2 = ord(t[i]) - sub
+
+            if maps[key1] != -1 and maps[key1] != key2:
                 return False
-            elif key2 in mapt and mapt[key2] != key1:
+            elif mapt[key2] != -1 and mapt[key2] != key1:
                 return False
             else:
                 maps[key1] = key2
                 mapt[key2] = key1
-            
+        
         return True
+        # maps = {}
+        # mapt = {}
+
+        # for i in range(n):
+        #     key1 = s[i]
+        #     key2 = t[i]
+            
+        #     if key1 in maps and maps[key1] != key2:
+        #         return False
+        #     elif key2 in mapt and mapt[key2] != key1:
+        #         return False
+        #     else:
+        #         maps[key1] = key2
+        #         mapt[key2] = key1
+            
+        # return True
