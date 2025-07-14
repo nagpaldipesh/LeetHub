61a4cfa4d81eca5303622adea8ead77e9fc4e504
nagpaldipesh <93336445+nagpaldipesh@users.noreply.github.com>
2025-07-14 12:07:44 +0530

    Time: 7 ms (84.92%), Space: 17.9 MB (88.02%) - LeetHub

--- 2025-redistribute-characters-to-make-all-strings-equal/2025-redistribute-characters-to-make-all-strings-equal.py
+++ 2025-redistribute-characters-to-make-all-strings-equal/2025-redistribute-characters-to-make-all-strings-equal.py
@@ -1,16 +1,27 @@
 class Solution:
     def makeEqual(self, words: List[str]) -> bool:
-        count = [0] * 26
-        hash = ord('a')
 
-        for word in words:
-            for c in word:
-                index = ord(c) - hash
-                count[index] += 1
-        
+        from collections import Counter
+
+        freq = Counter("".join(words))
         n = len(words)
-        for num in count:
-            if num % n != 0:
-                return False
 
+        for count in freq.values():
+            if count % n != 0:
+                return False
+        
         return True
+        # count = [0] * 26
+        # hash = ord('a')
+
+        # for word in words:
+        #     for c in word:
+        #         index = ord(c) - hash
+        #         count[index] += 1
+        
+        # n = len(words)
+        # for num in count:
+        #     if num % n != 0:
+        #         return False
+
+        # return True
