db954ba09c6c5524ca26c51024a0ea52ac89f762
nagpaldipesh <93336445+nagpaldipesh@users.noreply.github.com>
2025-07-14 13:11:11 +0530

    Time: 13 ms (58.03%), Space: 18.8 MB (92.14%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        duplicateNumber = -1
+
+        for num in nums:
+            index = abs(num) - 1
+
+            if nums[index] < 1:
+                duplicateNumber = index + 1
+            else:
+                nums[index] = -1 * nums[index]
+
+        for i, num in enumerate(nums):
+            if num > 0:
+                return [duplicateNumber, i + 1]
+
+        return [-1, -1]
