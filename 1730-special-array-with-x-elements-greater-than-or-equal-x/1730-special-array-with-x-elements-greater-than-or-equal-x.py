2d17d46201425ca637b9b22e459c5e6fc3ab217e
nagpaldipesh <93336445+nagpaldipesh@users.noreply.github.com>
2025-07-14 22:00:58 +0530

    Time: 3 ms (46.82%), Space: 17.8 MB (37.18%) - LeetHub

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.py
@@ -1,8 +1,13 @@
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
+        # nums.sort()
+        # n = len(nums)
+
+        # for i in range(1, nums[n-1] + 1):
+
         max_num = max(nums)
 
-        for i in range(1, max_num + 1):
+        for i in range(1, len(nums) + 1):
             equalOrGreater = 0
             for num in nums:
                 if num >= i:
@@ -12,3 +17,14 @@ class Solution:
                 return i
             
         return -1
+
+    # def get_lower_bount(self, num: List[int], n : int) -> int:
+    #     low = 0
+    #     high = n
+
+    #     idx = n
+        
+    #     while low <= high:
+    #         mid = low + (high - low) /2
+
+    #         if ()
