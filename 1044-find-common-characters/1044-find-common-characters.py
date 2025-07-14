cd425f8f83490f65431873f43c11c47c92553120
nagpaldipesh <93336445+nagpaldipesh@users.noreply.github.com>
2025-07-14 14:12:12 +0530

    Time: 11 ms (58.46%), Space: 17.9 MB (43.42%) - LeetHub

--- 1044-find-common-characters/1044-find-common-characters.py
+++ 1044-find-common-characters/1044-find-common-characters.py
@@ -1,27 +1,44 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
 
-        min_freq = [1000] * 26
-        sub = ord('a')
-        
         from collections import Counter
 
-        for word in words:
-            freq = Counter(word)
+        common = Counter(words[0])
 
-            for i in range(26):
-                c = chr(i+sub)
-                min_freq[i] = min(min_freq[i], freq[c])
+        for word in words[1:]:
+            common &= Counter(word)
 
         result = []
 
-        for i in range(26):
-            #print(freq)
-            freq = min_freq[i]
-            if freq > 0:
-                c = chr(i+sub)
-                result.extend(c * freq)
+        for (k, v) in common.items():
 
-        # return list(c if freq > 0 for i, freq in enumerate(min_freq))
+            result.extend(k*v)
 
         return result
+
+
+
+        # min_freq = [1000] * 26
+        # sub = ord('a')
+        
+        # from collections import Counter
+
+        # for word in words:
+        #     freq = Counter(word)
+
+        #     for i in range(26):
+        #         c = chr(i+sub)
+        #         min_freq[i] = min(min_freq[i], freq[c])
+
+        # result = []
+
+        # for i in range(26):
+        #     #print(freq)
+        #     freq = min_freq[i]
+        #     if freq > 0:
+        #         c = chr(i+sub)
+        #         result.extend(c * freq)
+
+        # # return list(c if freq > 0 for i, freq in enumerate(min_freq))
+
+        # return result
