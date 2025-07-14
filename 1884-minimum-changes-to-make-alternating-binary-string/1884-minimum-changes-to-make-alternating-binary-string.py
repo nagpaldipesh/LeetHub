5aa209769c1a6648f48cf23459e014f3e1ec112a
nagpaldipesh <93336445+nagpaldipesh@users.noreply.github.com>
2025-07-14 11:54:44 +0530

    Time: 11 ms (60.84%), Space: 18 MB (41.49%) - LeetHub

--- 1884-minimum-changes-to-make-alternating-binary-string/1884-minimum-changes-to-make-alternating-binary-string.py
+++ 1884-minimum-changes-to-make-alternating-binary-string/1884-minimum-changes-to-make-alternating-binary-string.py
@@ -4,12 +4,12 @@ class Solution:
         count = count1 = 0
 
         for i, c in enumerate(s):
-            #expected_1 = '0' if i % 2 == 0 else '1'
-            #expected_2 = '1' if i % 2 == 0 else '0'
+            expected_1 = '0' if i % 2 == 0 else '1'
+            expected_2 = '1' if i % 2 == 0 else '0'
 
-            if c != str(i % 2): 
+            if c != expected_1: 
                 count += 1
-            if c != str((i +1) % 2): 
+            if c != expected_2: 
                 count1 += 1
         
         return min(count, count1)
