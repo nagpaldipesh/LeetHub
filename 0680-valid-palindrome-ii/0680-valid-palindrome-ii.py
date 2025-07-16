bc4b9fb54e4b76bea8216d0b3339a251fbabf5f2
nagpaldipesh <93336445+nagpaldipesh@users.noreply.github.com>
2025-07-16 21:28:52 +0530

    Time: 64 ms (43.5%), Space: 18 MB (56.03%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,15 +1,13 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         
-        charDeleted = False
-
         start = 0
         end = len(s) - 1
         while start < end:
             if s[start] != s[end]:
                 return (
-                    self.validPalidrome(s, start+1, end) or
-                    self.validPalidrome(s, start, end - 1))
+                    self.isPalindrome(s, start+1, end) or
+                    self.isPalindrome(s, start, end - 1))
             
             start += 1
             end -= 1
@@ -17,7 +15,7 @@ class Solution:
         return True
     
 
-    def validPalidrome(self, s: str, start: int, end: int):
+    def isPalindrome(self, s: str, start: int, end: int) -> bool:
         
         while start < end:
             if s[start] != s[end]:
