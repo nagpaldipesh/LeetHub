0ae726aac3570c29ef20d08d461a3c61f0620710
nagpaldipesh <93336445+nagpaldipesh@users.noreply.github.com>
2025-07-17 22:24:56 +0530

    Time: 0 ms (100%), Space: 17.8 MB (76.46%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
@@ -4,7 +4,7 @@ class Solution:
             if nums[i] == nums[i-1]:
                 nums[i-1] , nums[i] = nums[i-1] * 2, 0
 
-        result = [num for num in nums if num > 0]
+        result = [num for num in nums if num != 0]
         
         result.extend([0] * (len(nums) - len(result)))
         return result
