01dc95d68beaebf750adfdf783dd573ec288036e
nagpaldipesh <93336445+nagpaldipesh@users.noreply.github.com>
2025-07-17 21:31:36 +0530

    Time: 3 ms (47.97%), Space: 18.5 MB (56.38%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.py
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.py
@@ -1,6 +1,17 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        filtered = [num for num in nums if num % 2 == 0]
-        filtered.extend([num for num in nums if num not in filtered])
+        result = []
 
-        return filtered
+        for num in nums:
+            if num % 2 == 0:
+                result.append(num)
+        
+        for num in nums:
+            if num % 2 == 1:
+                result.append(num)
+        
+        return result
+        # filtered = [num for num in nums if num % 2 == 0]
+        # filtered.extend([num for num in nums if num not in filtered])
+
+        # return filtered
