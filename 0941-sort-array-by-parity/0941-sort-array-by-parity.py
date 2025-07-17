a371846b2c5ef3186c2851aae3bffe8f46e93aec
nagpaldipesh <93336445+nagpaldipesh@users.noreply.github.com>
2025-07-17 21:35:02 +0530

    Time: 0 ms (100%), Space: 18.4 MB (82.16%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.py
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.py
@@ -1,16 +1,25 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        result = []
+        even_index = 0
 
-        for num in nums:
+        for i, num in enumerate(nums):
             if num % 2 == 0:
-                result.append(num)
+                nums[i], nums[even_index] = nums[even_index], nums[i]
+                even_index += 1
+            
+        return nums
+
+        # result = []
+
+        # for num in nums:
+        #     if num % 2 == 0:
+        #         result.append(num)
         
-        for num in nums:
-            if num % 2 == 1:
-                result.append(num)
+        # for num in nums:
+        #     if num % 2 == 1:
+        #         result.append(num)
         
-        return result
+        # return result
         # filtered = [num for num in nums if num % 2 == 0]
         # filtered.extend([num for num in nums if num not in filtered])
 
