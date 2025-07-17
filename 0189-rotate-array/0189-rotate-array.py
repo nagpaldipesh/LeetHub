a6d50513100200dc3b003334e044b22c893d90d5
nagpaldipesh <93336445+nagpaldipesh@users.noreply.github.com>
2025-07-18 00:50:47 +0530

    Time: 7 ms (44.76%), Space: 25.7 MB (20.76%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,9 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-
-        for i in range(k):
-            num = nums.pop()
-            nums.insert(0, num)
-
         
+        n = len(nums)
+
+        if k > n:
+            k %= n
+
+        self.rotate_by_indexes(nums, 0, n - 1)
+        self.rotate_by_indexes(nums, 0, k - 1)
+        self.rotate_by_indexes(nums, k, n - 1)
+        # for i in range(k):
+        #     num = nums.pop()
+        #     nums.insert(0, num)
+
+    
+    def rotate_by_indexes(self, nums: List[int], start: int, end: int) -> None:
+
+        while start < end:
+            nums[start] , nums [end] = nums[end], nums[start]
+            start += 1
+            end -= 1
