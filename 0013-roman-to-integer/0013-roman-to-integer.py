8b9b2b010d1bbfd4c54f07fa4e1a6dc79e08cb0b
nagpaldipesh <93336445+nagpaldipesh@users.noreply.github.com>
2025-07-19 22:55:00 +0530

    Time: 7 ms (40.73%), Space: 17.8 MB (40.1%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,23 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_map = {
+            'I' : 1,
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 500,
+            'M' : 1000
+        }
+
+        n = len(s)
+        num = roman_map[s[n - 1]]
+        for i in reversed(range(n -1)):
+            c = s[i]
+            next_char = s[i+1]
+            if ((c == 'I' and next_char in 'VX') or (c == 'X' and next_char in 'LC') or (c == 'C' and next_char in 'DM')):
+                num -= roman_map[c]
+            else:
+                num += roman_map[c]
+        
+        return num
