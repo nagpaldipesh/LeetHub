6f55be95943bcf6967cceee6ada9cd12b4c61143
nagpaldipesh <93336445+nagpaldipesh@users.noreply.github.com>
2025-09-21 12:32:19 +0530

    Time: 5 ms (50.88%), Space: 18.7 MB (95.2%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,10 +4,10 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-        nonZeroIndex = 0
+        indexToSwap = 0
 
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[i], nums[nonZeroIndex] = nums[nonZeroIndex], nums[i]
-                nonZeroIndex += 1
-            
+        for index, num in enumerate(nums):
+            if num != 0:
+                nums[index], nums[indexToSwap] = nums[indexToSwap], nums[index]
+                indexToSwap += 1
+        
