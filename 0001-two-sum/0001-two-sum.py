bbcc0d16a1d636e37d0f286a79328ecfd8f4a743
nagpaldipesh <93336445+nagpaldipesh@users.noreply.github.com>
2025-09-21 13:40:31 +0530

    Time: 0 ms (100%), Space: 19.1 MB (10.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,17 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        my_dict = dict()
-        for index, num in enumerate(nums):
-            complement  = target - num
-            if complement  in my_dict:
-                return [my_dict[complement],index]
-            my_dict[num] = index
         
-        return [-1,-1]
+        numIndex = {}
 
+        for index, num in enumerate(nums):
+
+            numToCheck = target - num
+
+            if numToCheck in numIndex:
+                return [numIndex[numToCheck], index]
+            
+            else:
+
+                numIndex[num] = index
+            
+        return (-1, -1)
